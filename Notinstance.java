96cf61c3a620c9035ea5591555312736a65f6aae
Makoto Tanabe <xekid78@outlook.com>
2018-03-22 15:29:22 +0900

    Add files via upload

---
+++ Notinstance.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Notinstance {
+
+	public static void main(String[] args) {
+		Calc3.method1();
+		Calc3.method2();
+
+	}
+
+}
+
+interface Calc3 {
+	public String STR = "足し算";
+	public int A = 7;
+	public int B = 8;
+
+	public static void method1() {
+		System.out.println(STR + "をします");
+	};
+
+	public static void method2() {
+		System.out.println(A + " + " + B + " は " + (A + B) + " です。");
+	}
+}
